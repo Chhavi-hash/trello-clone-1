11ee22fd75911d90020b3fd03a64c28fb843a3ba
Chhavi <chhavi123@gmail.com>
2026-03-29 01:55:19 +0530

    fix vite config properly

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_URL,
+        target: 'http://localhost:5001',
         changeOrigin: true,
         secure: false
       }
